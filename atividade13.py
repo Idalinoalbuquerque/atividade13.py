8b2caf799cebc071faf016b3231a9cf7508f0126
Italoo220 <italosilava76@gmail.com>
2023-10-23 14:07:36 -0300

    first commit

---
+++ atividade13.py
@@ -0,0 +1,17 @@
+# Exercício Python 14: Faça um programa que leia três números e
+# mostre qual é o maior e qual é o menor.
+a = int(input("Digite o primeiro valor:"))
+b = int(input("Digite o segundo valor:"))
+c = int(input("Digite o terceiro valor:"))
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+menor = a
+if b < c and b < a:
+    menor = b
+if c < b and c < a:
+    menor = c
+print(f"O menor número digitado foi {menor}")
+print(f"O maior número digitado foi {maior}")
